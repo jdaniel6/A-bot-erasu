2fa0479f7821566f631b987c63a1b4f49def6914
Jeffrey Daniel <78628314+jdaniel6@users.noreply.github.com>
2023-05-19 01:16:43 -0500

    updates commonfunctions.js
    adds functionality; generateSessionID isn't returning a string, only a Promise; and I have no idea why

--- commands/basic/latency.js
+++ commands/basic/latency.js
@@ -1,2 +1,11 @@
-const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
-interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('latency')
+        .setDescription('Gets the bot\'s latency'),
+    async execute(interaction) {
+        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+        interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+    },
+};
